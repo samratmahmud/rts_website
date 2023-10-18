2e99ada80a9f60e0bee03575342ae46d83d9d794
samratmahmud <samratmahmud1999@gmail.com>
2023-10-18 19:19:57 +0600

    rts

--- src/app/home/Card.tsx
+++ src/app/home/Card.tsx
@@ -8,10 +8,10 @@ function Card() {
       <div>
         <CardTop />
       </div>
-      <div className="mb-14 relative z-10">
+      <div className="lg:mb-14 relative z-10">
         <div
           aria-label="Cards 1"
-          className="lg:flex items-center mb-11 bg-card1 lg:[background-image:none] bg-no-repeat bg-cover px-4 lg:px-0"
+          className="lg:flex items-center lg:mb-11 bg-card1 lg:[background-image:none] bg-no-repeat bg-cover px-4 lg:px-0"
         >
           <div className="bg-emerald-600 md:pl-20 md:pr-12 px-5 md:py-20 pt-7 pb-14 lg:max-w-[602px] md:-mt-20 z-0 lg:ml-14 -translate-y-[20%] md:translate-y-0">
             <div className="md:text-19xl text-16xl font-bold text-white text-right md:mb-3 mb-1">
@@ -41,24 +41,27 @@ function Card() {
               alt=""
             />
           </div>
-          <div className="-mx-4 lg:hidden">
+          <div className="-mx-4 lg:hidden pb-[71px] -mt-[20%] md:-mt-0">
             <img className="w-full" src="/picture/Vector (10).svg" alt="" />
           </div>
         </div>
-        <div aria-label="Card 2" className="lg:relative">
-          <div className="lg:flex gap-20 items-center mb-24">
-            <div className="max-w-[482px] py-10 px-5 ml-24 col-span-5">
-              <div className="text-19xl font-bold text-emerald-600 mb-5">
+        <div
+          aria-label="Card 2"
+          className="lg:relative bg-card2 lg:[background-image:none] bg-no-repeat bg-cover"
+        >
+          <div className="lg:flex gap-20 items-center mb-24 -translate-y-[20%] lg:-translate-y-0">
+            <div className="lg:max-w-[482px] bg-white md:py-10 pt-[77px] pb-14 px-5 lg:ml-24 mx-4 lg:mx-0">
+              <div className="md:text-19xl text-16xl font-bold md:text-emerald-600 text-slate-900 md:mb-5 mb-2 text-right md:text-left">
                 2.
               </div>
-              <div className="text-lg font-medium text-emerald-600 mb-10">
-                RTS Solutions to <br /> #Addressing the current <br />{" "}
+              <div className="md:text-lg text-md font-medium text-emerald-600 md:mb-10 mb-7">
+                RTS Solutions to <br /> #Addressing the current <br />
                 challenges in textile recycling
               </div>
-              <div className="text-15xl font-medium text-slate-900 mb-5">
-                Monofilament With Greater Inclusivity{" "}
+              <div className="md:text-15xl text-9xl font-medium text-slate-900 mb-6">
+                Monofilament <br /> With Greater Inclusivity{" "}
               </div>
-              <div className="text-lg text-slate-900 mb-8 max-w-[402px]">
+              <div className="md:text-lg text-md text-slate-900 md:mb-8 mb-10 lg:max-w-[360px]">
                 Adopting a new method for mechanical recycling to overcome the
                 challenges posed by synthetic textiles.
               </div>
@@ -69,30 +72,39 @@ function Card() {
                 <Button name="Find out More" />
               </div>
             </div>
-            <div className="col-span-6">
+            <div className="col-span-6 hidden lg:block">
               <img src="/picture/RTS_RetainToSustain45 1.png" alt="" />
             </div>
           </div>
-          <div className="absolute -bottom-14">
+          <div className="absolute -bottom-14 hidden lg:block">
             <img className="w-screen" src="/picture/Vector.svg" alt="" />
           </div>
+          <div className="lg:hidden -mt-60 pb-24">
+            <img className="w-full" src="/picture/Vector (4).png" alt="" />
+          </div>
         </div>
-        <div aria-label="Cards 3" className="lg:flex items-center mb-3">
-          <div className="max-w-[604px] -ml-4">
+        <div
+          aria-label="Cards 3"
+          className="lg:flex items-center bg-card3 bg-no-repeat bg-cover lg:[background-image:none] lg:mr-24"
+        >
+          <div className="max-w-[604px hidden lg:block">
             <img
+              className="w-screen"
               src="/picture/martin-katler-Y4fKN-RlMV4-unsplash 9.png"
               alt=""
             />
           </div>
-          <div className="bg-slate-900 pl-20 pr-10 pt-7 pb-20 max-w-[602px]">
-            <div className="text-19xl font-bold text-white mb-7">3.</div>
-            <div className="text-lg font-medium text-emerald-600 mb-10">
+          <div className="bg-slate-900 md:pl-20 md:pr-10 px-5 pt-7 md:pb-20 pb-14 lg:max-w-[602px] mx-4 lg:mx-0 -translate-y-[13%] lg:-translate-y-0">
+            <div className="md:text-19xl text-16xl font-bold text-white md:mb-7 mb-2 text-right lg:text-left">
+              3.
+            </div>
+            <div className="md:text-lg md:text-md font-medium text-emerald-600 md:mb-10 mb-7">
               RTS Solutions to <br /> #Embracing a mono-material approach
             </div>
-            <div className="text-15xl font-medium text-white mb-5">
+            <div className="md:text-15xl text-9xl font-medium text-white mb-6">
               Monofilaments For Improved Recyclability
             </div>
-            <div className="text-lg text-white mb-14 max-w-[442px]">
+            <div className="text-lg text-white md:mb-14 mb-10 lg:max-w-[442px]">
               Mono-material designs that promote product recyclability.
             </div>
             <div
@@ -102,20 +114,26 @@ function Card() {
               <Button name="Find out More" />
             </div>
           </div>
+          <div className="pt-16 pb-9 lg:hidden">
+            <img className="w-full" src="/picture/Vector (4).png" alt="" />
+          </div>
         </div>
-        <div aria-label="Card 4" className="relative">
-          <div className="lg:flex gap-5 items-center mt-24">
-            <div className=" max-w-[482px] py-10 px-5 ml-6 col-span-5">
-              <div className="text-19xl font-bold text-emerald-600 mb-7">
+        <div
+          aria-label="Card 4"
+          className="lg:relative bg-card4 bg-no-repeat bg-cover lg:[background-image:none] pb-16"
+        >
+          <div className="lg:flex gap-5 items-center lg:mt-24">
+            <div className="lg:max-w-[482px] md:py-10 pb-[72px] pt-7 px-5 lg:ml-6 mx-4 lg:mx-0 bg-white -translate-y-[13%] lg:-translate-y-0">
+              <div className="md:text-19xl text-16xl font-bold lg:text-emerald-600 text-slate-900 md:mb-7 mb-2 text-right lg:text-left">
                 4.
               </div>
-              <div className="text-lg font-medium text-emerald-600 mb-12">
+              <div className="md:text-lg text-md font-medium text-emerald-600 md:mb-12 mb-7">
                 RTS Solutions to <br /> #Microfiber shedding
               </div>
-              <div className="text-15xl font-medium text-slate-900 mb-5">
+              <div className="md:text-15xl text-9xl font-medium text-slate-900 mb-6">
                 Monofilament With Higher Durability and Low Shedding
               </div>
-              <div className="text-lg text-slate-900 mb-8">
+              <div className="md:text-lg text-md text-slate-900 md:mb-8 mb-10">
                 Combating microfiber shedding with our fine, resilient
                 monofilament.
               </div>
@@ -126,11 +144,11 @@ function Card() {
                 <Button name="Find out More" />
               </div>
             </div>
-            <div className="mr-14 col-span-6">
+            <div className="mr-14 hidden lg:block">
               <img src="/picture/RTS_RetainToSustain12 1.png" alt="" />
             </div>
           </div>
-          <div className="absolute -top-24 -z-10">
+          <div className="absolute -top-24 -z-10 hidden lg:block">
             <img src="/picture/Vector (1).svg" alt="" />
           </div>
         </div>
